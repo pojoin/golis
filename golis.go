b3589c8862639b047376caba574103a8b9815904
hcq <hechuangqiang@gmail.com>
2015-03-13 14:01:59 +0800

    +golist.go

--- golis.go
+++ golis.go
@@ -1,6 +1,9 @@
 package golis
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -10,16 +13,15 @@ import (
 //系统变量定义
 
 var (
-	ioHandler IoHandler    //事件处理
-	unpacket  UnpacketType //协议解包函数
-	packet    PacketType   //协议封包函数
+	GolisHandler IoHandler //事件处理
+	GolisMsg     Message   //消息协议
 )
 
-//定义解包函数类型
-type UnpacketType func([]byte, chan interface{}) []byte
-
-//定义封包函数类型
-type PacketType func(interface{}) []byte
+//消息协议
+type Message interface {
+	Unpacket([]byte) interface{}
+	Packet(interface{}) []byte
+}
 
 //定义session
 type Iosession struct {
@@ -29,8 +31,10 @@ type Iosession struct {
 //session写入数据
 func (this *Iosession) Write(message *interface{}) {
 	//触发消息发送事件
-	ioHandler.MessageSent(this, message)
-	this.conn.Write(packet(message))
+	GolisHandler.MessageSent(this, message)
+	data := GolisMsg.Packet(message)
+	totalLen := len(data)
+	this.conn.Write(append(IntToBytes(totalLen), data...))
 }
 
 //事件触发接口定义
@@ -40,9 +44,9 @@ type IoHandler interface {
 	//session关闭
 	SessionClosed(session *Iosession)
 	//收到消息时触发
-	MessageReceived(session *Iosession, message *interface{})
+	MessageReceived(session *Iosession, message interface{})
 	//消息发送时触发
-	MessageSent(session *Iosession, message *interface{})
+	MessageSent(session *Iosession, message interface{})
 }
 
 //运行golis
@@ -68,22 +72,27 @@ func connectHandle(conn net.Conn) {
 	//声明一个临时缓冲区，用来存储被截断的数据
 	tmpBuffer := make([]byte, 0)
 	buffer := make([]byte, 1024)
-	readChannel := make(chan interface{}, 16)
 	//创建session
 	session := Iosession{conn}
 	//触发sessionCreated事件
-	ioHandler.SessionOpened(&session)
+	GolisHandler.SessionOpened(&session)
 
-	//通过readDataFromChannel监听客户发过来的数据
-	go readDataFromChannel(&session, readChannel)
 	flag := true
 	for flag {
 		n, err := conn.Read(buffer)
 		switch err {
 		case nil:
-			tmpBuffer = unpacket(append(tmpBuffer, buffer[:n]...), readChannel)
+			tmp, data, err := getReadyData(append(tmpBuffer, buffer[:n]...))
+			tmpBuffer = tmp
+			if err != nil {
+				Log(err.Error())
+			} else {
+				readFromData(&session, data)
+			}
 		case io.EOF:
 			Log("client is disconnected")
+			//session关闭
+			GolisHandler.SessionClosed(&session)
 			flag = false
 			break
 		default:
@@ -93,14 +102,45 @@ func connectHandle(conn net.Conn) {
 
 }
 
-func readDataFromChannel(session *Iosession, readChannel chan interface{}) {
-	for {
-		select {
-		case data := <-readChannel:
-			//收到消息时到达
-			ioHandler.MessageReceived(session, &data)
+//协议中查看协议头是否满足一个协议报
+func getReadyData(buffer []byte) ([]byte, []byte, error) {
+	length := len(buffer)
+	if length >= 32 {
+		totalLen := BytesToInt(buffer[0:32]) //get totalLen
+		if totalLen == 0 {
+			return make([]byte, 0), nil, errors.New("msg is null")
+		} else if totalLen <= length-32 {
+			return buffer[totalLen+32:], buffer[32:totalLen], nil
 		}
+
 	}
+	return buffer, nil, errors.New("msg is not ready")
+}
+
+//从准备好的数据读取
+func readFromData(session *Iosession, data []byte) {
+	message := GolisMsg.Unpacket(data) //拆包
+	//收到消息时到达
+	GolisHandler.MessageReceived(session, message)
+}
+
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
 
 //简单日志输出
