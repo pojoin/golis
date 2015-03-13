dcb743934f729ae52e76a12a0789ca1374057021
hcq <hechuangqiang@gmail.com>
2015-03-13 16:45:00 +0800

    *修改粘报问题

--- golis.go
+++ golis.go
@@ -67,23 +67,29 @@ func connectHandle(conn net.Conn) {
 	//声明一个临时缓冲区，用来存储被截断的数据
 	tmpBuffer := make([]byte, 0)
 	buffer := make([]byte, 1024)
+
+	//声明一个管道用于接收解包的数据
+	readerChannel := make(chan []byte, 16)
 	//创建session
 	session := Iosession{conn}
 	//触发sessionCreated事件
 	GolisHandler.SessionOpened(&session)
+
+	go reader(&session, readerChannel)
 
 	flag := true
 	for flag {
 		n, err := conn.Read(buffer)
 		switch err {
 		case nil:
-			tmp, data, err := getReadyData(append(tmpBuffer, buffer[:n]...))
-			tmpBuffer = tmp
-			if err != nil {
-				Log(err.Error())
-			} else {
-				readFromData(&session, data)
-			}
+			//			tmp, data, err := getReadyData(append(tmpBuffer, buffer[:n]...))
+			//			tmpBuffer = tmp
+			//			if err != nil {
+			//				Log(err.Error())
+			//			} else {
+			//				readFromData(&session, data)
+			//			}
+			tmpBuffer = unpack(append(tmpBuffer, buffer[:n]...), readerChannel)
 		case io.EOF:
 			Log("client is disconnected")
 			//session关闭
@@ -97,19 +103,57 @@ func connectHandle(conn net.Conn) {
 
 }
 
+const (
+	constDataLength = 4
+)
+
+//解包
+func unpack(buffer []byte, readerChannel chan []byte) []byte {
+	length := len(buffer)
+
+	var i int
+	for i = 0; i < length; i = i + 1 {
+		if length < i+constDataLength {
+			break
+		}
+		messageLength := BytesToInt(buffer[i : i+constDataLength])
+		if length < i+constDataLength+messageLength {
+			break
+		}
+		data := buffer[i+constDataLength : i+constDataLength+messageLength]
+		readerChannel <- data
+		i += constDataLength + messageLength - 1
+	}
+
+	if i == length {
+		return make([]byte, 0)
+	}
+	return buffer[i:]
+}
+
 //协议中查看协议头是否满足一个协议报
 func getReadyData(buffer []byte) ([]byte, []byte, error) {
 	length := len(buffer)
-	if length >= 32 {
-		totalLen := BytesToInt(buffer[0:32]) //get totalLen
+	Log("length = ", length)
+	if length >= 4 {
+		totalLen := BytesToInt(buffer[0:4]) //get totalLen
 		if totalLen == 0 {
 			return make([]byte, 0), nil, errors.New("msg is null")
-		} else if totalLen <= length-32 {
-			return buffer[totalLen+32:], buffer[32:totalLen], nil
+		} else if totalLen <= length-4 {
+			return buffer[totalLen+4:], buffer[4:totalLen], nil
 		}
 
 	}
 	return buffer, nil, errors.New("msg is not ready")
+}
+
+func reader(session *Iosession, readerChannel chan []byte) {
+	for {
+		select {
+		case data := <-readerChannel:
+			readFromData(session, data)
+		}
+	}
 }
 
 //从准备好的数据读取
